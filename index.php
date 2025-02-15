cfad8c6f303db0d48ada94bad534dd297af87b48
Tyziryx <alexim13550@gmail.com>
2025-02-15 11:32:00 +0100

    maj page

--- index.php
+++ index.php
@@ -1,5 +1,22 @@
 <?php
-echo "Hello World!";
-?>
+// Vérifier si un paramètre 'page' est présent dans l'URL
+if (isset($_GET['page'])) {
+    $page = $_GET['page'];
 
-<!-- CHMOD 644 pour tous les dossiers web -->
+    // Définition des chemins autorisés
+    $routes = [
+        'home' => 'home/index.html',
+        'login' => 'app/templates/login.html',
+        'contact' => 'home/controllers/contact.html',
+    ];
+
+    // Vérifier si la page demandée existe
+    if (array_key_exists($page, $routes) && file_exists($routes[$page])) {
+        include $routes[$page]; // Inclut directement le fichier sans rediriger
+    } else {
+        include 'home/index.html'; // Page d'accueil par défaut
+    }
+} else {
+    include 'home/index.html'; // Redirection vers l'accueil si aucun paramètre
+}
+?>
