5d8b94bf889f6bee3a5232a5573963da5d52c906
Tyziryx <alexim13550@gmail.com>
2025-02-17 11:41:02 +0100

    modif index

--- app/index.php
+++ app/index.php
@@ -2,21 +2,16 @@
 <?php include 'templates/header.php'; ?>
 
 <?php
-if (!isset($_GET['page'])) {
-    header("Location: index.php");  // Redirige vers home/index.php (relatif au répertoire actuel)
-    exit;
-}
+
 // Récupérer la page, ou définir 'home' par défaut si la page n'est pas spécifiée
 $page = $_GET['page'] ?? 'login';
 
 // Définition des routes
 $routes = [
-    'home' => 'templates/home.php',
     'login' => 'templates/login.php',
     'geodex' => 'templates/geodex.php',
     'dashboard' => 'templates/dashboard.php',
-    'contact' => 'templates/contact.php',
-    'faq' => 'templates/faq.php',
+
 ];
 
 // Vérifier si la page demandée est dans les routes définies et que le fichier existe
@@ -27,4 +22,3 @@ if (array_key_exists($page, $routes) && file_exists($routes[$page])) {
 }
 ?>
 
-<?php include 'templates/footer.php'; ?>
