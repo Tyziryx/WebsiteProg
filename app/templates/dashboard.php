4a8512c8e9735d2790c2bfc6a9a1df540e8bc2d5
Tyziryx <alexim13550@gmail.com>
2025-02-17 11:16:51 +0100

    modif

---
+++ app/templates/dashboard.php
@@ -0,0 +1,13 @@
+<div class="container">
+        <h1>Tirage au sort</h1>
+        <p>Clique sur l'image pour obtenir un résultat !</p>
+        <div class="card">
+            <img id="geode" src="../images/geode.gif" alt="openable Geode">
+        </div>
+
+    </div>
+
+    <script src="js/geode.js"></script>
+</body>
+
+</html>
---
+++ app/templates/header.php
@@ -0,0 +1,35 @@
+<header>
+    <!-- Réccupéré sur codepen.io : https://codepen.io/themrsami/details/ogvedxR -->
+    <nav class="navbar">
+        <div class="navbar-container">
+            <div class="logo">Géodex</div>
+            <button class="mobile-nav-toggle" aria-label="Toggle navigation">
+                <span class="bar"></span>
+                <span class="bar"></span>
+                <span class="bar"></span>
+            </button>
+            <ul class="nav-links">
+                <?php
+                // Récupérer la page actuelle
+                $current_page = $_GET['page'] ?? 'home';
+
+                // Tableau des pages avec leurs liens
+                $pages = [
+                    'home' => 'Accueil',
+                    'contact' => 'Contact',
+                    'faq' => 'FAQ',
+                ];
+
+                // Parcourir les pages pour générer les liens
+                foreach ($pages as $page => $label) {
+                    $active_class = ($current_page == $page) ? 'active' : ''; // Ajouter la classe active si c'est la page actuelle
+                    echo "<li class=\"$active_class\"><a href=\"index.php?page=$page\">$label</a></li>";
+                }
+
+                // Lien spécifique pour Geodex
+                echo '<li><a href="../app/index.php">Geodex</a></li>';
+                ?>
+            </ul>
+        </div>
+    </nav>
+</header>
