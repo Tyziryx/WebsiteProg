9efea4d5cce0ab19bd573abc7d500cb09f323548
Qwantike <paul.moinereau@hotmail.fr>
2025-03-01 15:41:15 +0100

    ajout bouton accueil confirmation.php

--- templates/confirmation.php
+++ templates/confirmation.php
@@ -16,6 +16,7 @@
                 ?>
             </p>
             <a href="/AMS/control/contact.php" class="back-link">Retourner au formulaire</a>
+            <a href="/AMS/" class="back-link">Retourner à l'accueil</a>
         </div>
     </section>
 </main>
