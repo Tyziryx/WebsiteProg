48209ed45d0e2f13e51c36912a656c0772f44a7a
Tyziryx <alexim13550@gmail.com>
2025-03-02 14:54:38 +0100

    encore

--- index.php
+++ index.php
@@ -6,5 +6,7 @@ $racine_path = './';
 <?php include($racine_path.'control/home.php');?>
 
 <?php
-phpinfo();
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 ?>
