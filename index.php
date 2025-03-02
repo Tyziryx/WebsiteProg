5753a8997c15e83e22bd40242b3aaf09cd123277
Tyziryx <alexim13550@gmail.com>
2025-03-02 14:55:55 +0100

    encore

--- index.php
+++ index.php
@@ -3,10 +3,5 @@ $titre = 'Accueil';
 $racine_path = './';
 ?>
 
-<?php include($racine_path.'control/home.php');?>
+<?php include($racine_path.'/control/home.php');?>
 
-<?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-?>
