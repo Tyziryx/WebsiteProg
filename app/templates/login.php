31061cec8b56bd2e2eb4a7305a3acedafcb6800f
Tyziryx <alexim13550@gmail.com>
2025-03-04 21:36:17 +0100

    commit

--- app/templates/login.php
+++ app/templates/login.php
@@ -1,23 +1,21 @@
-
 <main>
         <section class="forms-section">
-
             <div class="forms">
                 <div class="form-wrapper is-active">
                     <button type="button" class="switcher switcher-login">
                         Login
                         <span class="underline"></span>
                     </button>
-                    <form class="form form-login" action="/app/models/process_login.php" method="POST">
+                    <form class="form form-login" action="<?php echo $racine_path . 'app/models/process_login.php'; ?>" method="POST">
                         <fieldset>
                             <legend>Please, enter your email and password for login.</legend>
                             <div class="input-block">
                                 <label for="login-email">E-mail</label>
-                                <input id="login-email" type="email" name="email" <?php /*required */ ?>>
+                                <input id="login-email" type="email" name="email" required>
                             </div>
                             <div class="input-block">
                                 <label for="login-password">Password</label>
-                                <input id="login-password" type="password" name="password" <?php /*required */ ?>>
+                                <input id="login-password" type="password" name="password" required>
                             </div>
                         </fieldset>
                         <button type="submit" class="btn-login">Login</button>
@@ -28,20 +26,20 @@
                         Sign Up
                         <span class="underline"></span>
                     </button>
-                    <form class="form form-signup">
+                    <form class="form form-signup" action="<?php echo $racine_path . 'app/models/process_signup.php'; ?>" method="POST">
                         <fieldset>
                             <legend>Please, enter your email, password and password confirmation for sign up.</legend>
                             <div class="input-block">
                                 <label for="signup-email">E-mail</label>
-                                <input id="signup-email" type="email" required>
+                                <input id="signup-email" type="email" name="email" required>
                             </div>
                             <div class="input-block">
                                 <label for="signup-password">Password</label>
-                                <input id="signup-password" type="password" required>
+                                <input id="signup-password" type="password" name="password" required>
                             </div>
                             <div class="input-block">
                                 <label for="signup-password-confirm">Confirm password</label>
-                                <input id="signup-password-confirm" type="password" required>
+                                <input id="signup-password-confirm" type="password" name="password_confirm" required>
                             </div>
                         </fieldset>
                         <button type="submit" class="btn-signup">Continue</button>
@@ -50,7 +48,7 @@
             </div>
         </section>
     </main>
-    <script src="/app/templates/js/login.js"></script>
+    <script src="<?php echo $racine_path . 'app/templates/js/login.js'; ?>"></script>
 </body>
 
 </html>
