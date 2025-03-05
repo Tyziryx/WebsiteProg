6e00f75070f07013ae6eeed17ca2a8ddbdd9e8b6
Tyziryx <tyziryx@gmail.com>
2025-03-05 16:02:00 +0100

    debut adin

---
+++ admin/index.php
@@ -0,0 +1,37 @@
+<?php
+$racine_path = './';
+?>
+
+<?php
+// session_start();
+
+$page = $_GET['page'] ?? 'login';
+
+// Liste des pages accessibles uniquement aux utilisateurs connectés
+$protected_pages = ['geodex', 'dashboard'];
+
+// Vérifie si l'utilisateur tente d'accéder à une page protégée sans être connecté
+if (in_array($page, $protected_pages) && !isset($_SESSION['user_id'])) {
+    header("Location: /public_html//app/control/login.php");
+    exit;
+}
+
+include 'templates/head.php';
+
+$routes = [
+    'login' => 'templates/login.php',
+    'geodex' => 'templates/geodex.php',
+    'dashboard' => 'templates/dashboard.php',
+];
+
+if ($page != 'login') {
+    include 'templates/header.php';
+    include 'templates/sidebar.php';
+}
+
+if (array_key_exists($page, $routes) && file_exists($routes[$page])) {
+    include $routes[$page];
+} else {
+    include 'templates/404.php';
+}
+?>
---
+++ admin/templates/sidebar.php
@@ -0,0 +1,11 @@
+<aside class="sidebar">
+    <div class="sidebar-header">Menu</div>
+    <nav>
+        <ul>
+            <li><a href="dashboard.php">Dashboard</a></li>
+            <li><a href="geodex.php">Géodex</a></li>
+            <li><a href="profil.php">Profil</a></li>
+            <li><a href="../models/logout.php">Déconnexion</a></li>
+        </ul>
+    </nav>
+</aside>
